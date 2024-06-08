466d38184a37abe0ff585bb8b63335f7f8e1fd94
Anshuman Sharma <anshuman9998@gmail.com>
2024-06-08 10:29:16 +0530

    initial commit

---
+++ Technology Feasta/feasta/android/app/src/main/kotlin/com/example/feasta/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.feasta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Technology Feasta/feasta/lib/src/presentation/screens/ticket_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TicketScreen extends StatefulWidget {
+  const TicketScreen({super.key});
+
+  @override
+  State<TicketScreen> createState() => _TicketScreenState();
+}
+
+class _TicketScreenState extends State<TicketScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("hello"),
+      ),
+    );
+  }
+}
